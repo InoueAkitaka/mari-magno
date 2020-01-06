cf56db7b25c3515c8159de4d5f54f92194554819
InoueAkitaka <inoue.akitaka@mari-magno.jp>
2020-01-06 20:02:28 +0900

    no message

--- index.php
+++ index.php
@@ -199,7 +199,7 @@ foreach ($events as $event) {
 			// 変更は工事中
 			//new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder ('出勤時間を変更する', 'B,' . $userSrg),
 			
-			$urlCSV = 'https://test-app-csv-mari-magno.herokuapp.com/?page=125';
+			$urlCSV = 'https://test-app-csv-mari-magno.herokuapp.com/';
 			
 			//$urlCSV = $uslCSV . $userSrg;
 			
