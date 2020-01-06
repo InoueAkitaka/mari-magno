a0929287703219c206766f3657f6a5c6d2d02bb2
InoueAkitaka <inoue.akitaka@mari-magno.jp>
2020-01-06 20:13:30 +0900

    no message

--- index.php
+++ index.php
@@ -190,7 +190,7 @@ foreach ($events as $event) {
 		//$bot->replyText($event->getReplyToken(), "出勤！！！");
 		$urlCSV = 'https://test-app-csv-mari-magno.herokuapp.com/?page=';
 
-		$urlCSV = $uslCSV . (string)$userSrg;
+		$urlGetCSV = $uslCSV . (string)$userSrg;
 
 		replyButtonsTemplate(
 			$bot,
@@ -202,7 +202,7 @@ foreach ($events as $event) {
 			new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder ('出勤する', 'A,' . $userSrg),
 			// 変更は工事中
 			//new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder ('出勤時間を変更する', 'B,' . $userSrg),
-			new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder ('Webで見る', $urlCSV),
+			new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder ('Webで見る', $urlGetCSV),
 			new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder ('CSV出力', 'B,' . $userSrg)
 		);
 
